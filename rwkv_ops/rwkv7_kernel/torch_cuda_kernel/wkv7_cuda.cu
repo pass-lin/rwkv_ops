72d94752a91c297f9bc6bc8d0d00d52cef3a18de
pass_lin <935499957@qq.com>
2025-10-04 14:39:36 +0800

    format cpp

--- rwkv_ops/rwkv7_kernel/torch_cuda_kernel/wkv7_cuda.cu
+++ rwkv_ops/rwkv7_kernel/torch_cuda_kernel/wkv7_cuda.cu
@@ -2,16 +2,23 @@
 #include <assert.h>
 
 using bf = __nv_bfloat16;
-__device__ inline float to_float(const bf & u) { return __bfloat162float(u); }
-__device__ inline bf to_bf(const float & u) { return __float2bfloat16_rn(u); }
 
+__device__ inline float to_float(const bf & u) {
+    return __bfloat162float(u);
+}
+
+__device__ inline bf to_bf(const float & u) {
+    return __float2bfloat16_rn(u);
+}
 typedef bf * __restrict__ F_;
 
 __global__ void forward_kernel(int T, int H, F_ w_, F_ q_, F_ k_, F_ v_, F_ a_, F_ b_, bf* y_, float* s_, float* sa_) {
     constexpr int C = _C_;
     int bb = blockIdx.y, hh = blockIdx.x, i = threadIdx.x;
-
-    float state[C] = {0};
+    float state[C] =  {
+        0
+    }
+    ;
     __shared__ float q[C], k[C], w[C], a[C], b[C];
 
     for (int t = 0; t < T; t++) {
@@ -23,17 +30,17 @@ __global__ void forward_kernel(int T, int H, F_ w_, F_ q_, F_ k_, F_ v_, F_ a_, 
         a[i] = to_float(a_[ind]);
         b[i] = to_float(b_[ind]);
         __syncthreads();
-
         float sa = 0;
-#pragma unroll
+        #pragma unroll
+
         for (int j = 0; j < C; j++) {
             sa += a[j] * state[j];
         }
         sa_[ind] = sa;
-
         float v = to_float(v_[ind]);
         float y = 0;
-#pragma unroll
+        #pragma unroll
+
         for (int j = 0; j < C; j++) {
             float& s = state[j];
             s = s * w[j] + sa * b[j] + k[j] * v;
@@ -43,19 +50,29 @@ __global__ void forward_kernel(int T, int H, F_ w_, F_ q_, F_ k_, F_ v_, F_ a_, 
 
         if ((t+1)%_CHUNK_LEN_ == 0) {
             int base = (bb*H+hh)*(T/_CHUNK_LEN_)*C*C + (t/_CHUNK_LEN_)*C*C + i;
-#pragma unroll
+            #pragma unroll
+
             for (int j = 0; j < C; j++) {
                 s_[base + j*C] = state[j];
             }
         }
     }
 }
-
-__global__ void backward_kernel(int T, int H, F_ w_, F_ q_, F_ k_, F_ v_, F_ a_, F_ b_, F_ dy_, float * __restrict__ s_, float * __restrict__ sa_, bf* dw_, bf* dq_, bf* dk_, bf* dv_, bf* da_, bf* db_) {
+__global__ void backward_kernel(int T, int H, F_ w_, F_ q_, F_ k_, F_ v_, F_ a_, F_ b_, F_ dy_,
+float * __restrict__ s_, float * __restrict__ sa_,
+bf* dw_, bf* dq_, bf* dk_, bf* dv_, bf* da_, bf* db_) {
     constexpr int C = _C_;
     int bb = blockIdx.y, hh = blockIdx.x, i = threadIdx.x;
-
-    float stateT[C] = {0}, dstate[C] = {0}, dstateT[C] = {0};
+    float stateT[C] =  {
+        0
+    }
+    , dstate[C] =  {
+        0
+    }
+    , dstateT[C] =  {
+        0
+    }
+    ;
     __shared__ float w[C], q[C], k[C], v[C], a[C], b[C], dy[C], sa[C], dSb_shared[C];
     float qi, wi, ki, ai, bi, dyi;
 
@@ -75,29 +92,30 @@ __global__ void backward_kernel(int T, int H, F_ w_, F_ q_, F_ k_, F_ v_, F_ a_,
 
         if ((t+1)%_CHUNK_LEN_ == 0) {
             int base = (bb*H+hh)*(T/_CHUNK_LEN_)*C*C + (t/_CHUNK_LEN_)*C*C + i*C;
-#pragma unroll
+            #pragma unroll
+
             for (int j = 0; j < C; j++) {
                 stateT[j] = s_[base + j];
             }
         }
-
         float dq = 0;
-#pragma unroll
+        #pragma unroll
+
         for (int j = 0; j < C; j++) {
             dq += stateT[j]*dy[j];
         }
         dq_[ind] = to_bf(dq);
-
         float iwi = 1.0f/wi;
-#pragma unroll        
+        #pragma unroll
+
         for (int j = 0; j < C; j++) {
             stateT[j] = (stateT[j] - ki*v[j] - bi*sa[j]) * iwi;
             dstate[j] += dyi * q[j];
             dstateT[j] += qi * dy[j];
         }
-
         float dw = 0, dk = 0, dv = 0, db = 0, dSb = 0;
-#pragma unroll
+        #pragma unroll
+
         for (int j = 0; j < C; j++) {
             dw += dstateT[j]*stateT[j];
             dk += dstateT[j]*v[j];
@@ -109,19 +127,18 @@ __global__ void backward_kernel(int T, int H, F_ w_, F_ q_, F_ k_, F_ v_, F_ a_,
         dk_[ind] = to_bf(dk);
         dv_[ind] = to_bf(dv);
         db_[ind] = to_bf(db);
-
         __syncthreads();
         dSb_shared[i] = dSb;
         __syncthreads();
-
         float da = 0;
-#pragma unroll
+        #pragma unroll
+
         for (int j = 0; j < C; j++) {
             da += stateT[j]*dSb_shared[j];
         }
         da_[ind] = to_bf(da);
+        #pragma unroll
 
-#pragma unroll        
         for (int j = 0; j < C; j++) {
             dstate[j] = dstate[j]*w[j] + dSb * a[j];
             dstateT[j] = dstateT[j]*wi + ai * dSb_shared[j];
@@ -132,6 +149,7 @@ __global__ void backward_kernel(int T, int H, F_ w_, F_ q_, F_ k_, F_ v_, F_ a_,
 void cuda_forward(int B, int T, int H, bf*w, bf*q, bf*k, bf*v, bf*z, bf*a, bf*y, float*s, float*sa) {
     forward_kernel<<<dim3(H,B), dim3(_C_)>>>(T,H,w,q,k,v,z,a,y,s,sa);
 }
+
 void cuda_backward(int B, int T, int H, bf*w, bf*q, bf*k, bf*v, bf*z, bf*a, bf*dy, float*s, float*sa, bf*dw, bf*dq, bf*dk, bf*dv, bf*dz, bf*da) {
     assert(T%_CHUNK_LEN_ == 0);
     backward_kernel<<<dim3(H,B), dim3(_C_)>>>(T,H,w,q,k,v,z,a,dy,s,sa,dw,dq,dk,dv,dz,da);
